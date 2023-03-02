01e32b863d90eba52009654c0c560f91eb3cc25f
WALID DRAÂ <89532247+wa1ead@users.noreply.github.com>
2023-03-02 13:00:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  //SET HEADER CONTENT-TYPE
+  res.setHeader("Content-Type", "text/html");
+
+  // res.write("<head><link rel='stylesheet' href='#'></head>");
+  // res.write("<p>HELLO RESPONSE</p>");
+  // res.write("<p>HELLO AGAIN</p>");
+  // res.end();
+
+  //SEND A HTML FILE
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
