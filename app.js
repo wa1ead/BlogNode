e3f8482a55106f7e773ddf75373018aa6f2f9212
WALID DRAÂ <89532247+wa1ead@users.noreply.github.com>
2023-03-03 12:55:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.send("<p>HOME PAGE</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
-  res.send("<p>ABOUT PAGE</p>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+//REDIRECTS
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
+});
+
+//404 PAGE (ALWAYS IN THE BOTTOM)
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
